fca1ad9290eecc91ecf283551717fd4361bf7d75
dilipSD97 <dilipakshykarthik1997@gmail.com>
2021-02-20 18:52:33 +0530

    added nav bar and alerts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainNavBarComponent } from './main-nav-bar/main-nav-bar.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { RegisterUserComponent } from './register-user/register-user.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavBarComponent,
+    HomePageComponent,
+    RegisterUserComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
